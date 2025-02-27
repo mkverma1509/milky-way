97a127913d82883a49a607e955837ce1e4daf651
mkverma1509 <mkverma1509@gmail.com>
2025-02-27 21:16:36 +0530

    second push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
 function Home() {
-  return <h1>Welcome to My Website</h1>;
+  return (
+  <body>
+  <h1>Welcome to My Website</h1>
+  <p>It is getting developed....</p>
+  <br/>
+  <b>Developer: Mahesh Kumar</b>
+  </body>
+  );
 }
 
 export default Home;
